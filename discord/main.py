f1157b6090a908f68ebce6373932cc5cc1338a38
João Vitor <joaoezeki@gmail.com>
2025-04-03 21:38:56 -0300

    discord V 0.1

--- discord/main.py
+++ discord/main.py
@@ -1,3 +1,39 @@
+import requests
+
+import discord
+from discord.ext import commands
+
+#from deep_translator import GoogleTranslator
+#from atproto import Client
 
 
-print("Hello!!")
+intents - discord.Intents.all()
+bot = commands.Bot(".", intents=intents)
+
+@bot.event
+async def on_ready():
+  print("Bot Iniciado!!")
+
+
+
+
+
+def fetch_nasa_media_data(api_key):
+  #"""Obtém os dados da mídia do dia da API da NASA."""
+  response = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={api_key}")
+  if response.status_code == 200:
+    data = response.json()
+    return {
+      'copyright': data['copyright'],
+      'date': data['date'],
+      'url': data['url'],
+      'title': data['title']
+    }
+  else:
+    print("Falha ao obter dados da API da NASA.")
+    return None
+
+
+
+
+bot.run()
